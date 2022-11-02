f3ef5d50fe6aa3ccb719df879af68da04db5fb1d
Albert <AccretionDiskThat@Gmail.com>
2022-11-02 19:28:45 +0500

    try push in github without config

---
+++ src/main/java/org/telegram/Tests/MyBot.java
@@ -0,0 +1,34 @@
+package org.telegram.Tests;
+
+import org.telegram.Config.BotConfig;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+public class MyBot extends TelegramLongPollingBot {
+    @Override
+    public String getBotUsername() {
+        return BotConfig.BOT_NAME;
+    }
+
+    @Override
+    public String getBotToken() {
+        return BotConfig.BOT_TOKEN;
+    } // add in gitignore !!!!
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            SendMessage message = new SendMessage();
+            message.setChatId(update.getMessage().getChatId().toString());
+            message.setText(update.getMessage().getText());
+
+            try {
+                execute(message);
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
